77cb9dbc10f4d778984ff9beaec94156d8c051ab
Yasmine <yasminedwedar30@gmail.com>
2020-04-07 00:27:42 +0200

    ........

--- app/src/main/java/com/vaatu/tripmate/utils/alarmManagerReciever/MyRingingService.java
+++ app/src/main/java/com/vaatu/tripmate/utils/alarmManagerReciever/MyRingingService.java
@@ -1,37 +1,37 @@
-package com.vaatu.tripmate.utils.alarmManagerReciever;
-
-import android.app.Service;
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.os.IBinder;
-import android.widget.Toast;
-
-import com.vaatu.tripmate.R;
-
-public class MyRingingService extends Service {
-
-    MediaPlayer player;
-
-    public IBinder onBind(Intent intent) {
-
-        return null;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-
-        Toast.makeText(MyRingingService.this, "music plyn from on start command ", Toast.LENGTH_LONG).show();
-        player = MediaPlayer.create(this, R.raw.sound);
-        player.setLooping(true); // Set looping
-        player.setVolume(100, 100);
-        player.start();
-
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-
-    public  void stopmusic(){
-
-        player.stop();
-    }
-}
+//package com.vaatu.tripmate.utils.alarmManagerReciever;
+//
+//import android.app.Service;
+//import android.content.Intent;
+//import android.media.MediaPlayer;
+//import android.os.IBinder;
+//import android.widget.Toast;
+//
+//import com.vaatu.tripmate.R;
+//
+//public class MyRingingService extends Service {
+//
+//    MediaPlayer player;
+//
+//    public IBinder onBind(Intent intent) {
+//
+//        return null;
+//    }
+//
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//
+//        Toast.makeText(MyRingingService.this, "music plyn from on start command ", Toast.LENGTH_LONG).show();
+//        player = MediaPlayer.create(this, R.raw.sound);
+//        player.setLooping(true); // Set looping
+//        player.setVolume(100, 100);
+//        player.start();
+//
+//        return super.onStartCommand(intent, flags, startId);
+//    }
+//
+//
+//    public  void stopmusic(){
+//
+//        player.stop();
+//    }
+//}
